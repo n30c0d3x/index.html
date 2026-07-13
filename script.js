85863c25c37e4684dca31edeef417a4b737186b5
n30c0d3x <neocodex010@gmail.com>
2026-07-12 21:19:44 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let codigo = "";
+
+document.addEventListener("keydown", function(event) {
+
+    codigo += event.key.toLowerCase();
+
+    if (codigo.length > 20) {
+        codigo = codigo.slice(-20);
+    }
+
+    if (codigo.endsWith("2011")) {
+        window.location.href = "easter.html";
+    }
+
+});
